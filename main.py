148372c65a9ad1d1efe3301fd7e08f700a9f8abc
HristoRaykov <hristocr@gmail.com>
2019-07-07 16:10:38 +0300

    Working on spacial data and networks homework.

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+import networkx as nx
+os.environ["PROJ_LIB"] = r"/home/hristocr/anaconda3/envs/jupyter-notebook/share/proj"
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
